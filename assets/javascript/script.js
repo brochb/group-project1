67b092e54ab585407e7f4f5f67a3b2b2005623c1
Brochb <brochbaltzer@gmail.com>
2023-08-04 20:26:42 -0600

    added variables to javascript

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,6 +2,11 @@ var myHeaders = new Headers();
 myHeaders.append("X-RapidAPI-Key", "203d6f8221msh723786e2656b6a5p1512adjsn9cc9321e6473");
 myHeaders.append("Cookie", "caf_ipaddr=35.162.152.183; city=\"Boardman\"; country=US; system=PW; traffic_target=reseller");
 
+var searchContainer = document.getElementById("search-container")
+var regionSearch = document.getElementById("text-box-1")
+var ingredientsSearch = document.getElementById("text-box-2")
+var searchButton = document.getElementById("search-button")
+
 var requestOptions = {
   method: 'GET',
   headers: myHeaders,
