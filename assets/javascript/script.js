988fd42ea9c36c5694bf3bf68a7e7178f16f6179
Richard-Au1 <136401843+Richard-Au1@users.noreply.github.com>
2023-08-14 19:52:56 -0600

    pulling

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,6 @@
 var apiUrl;
 var myHeaders = new Headers();
-myHeaders.append("X-RapidAPI-Key", "203d6f8221msh723786e2656b6a5p1512adjsn9cc9321e6473");
+myHeaders.append("X-RapidAPI-Key", "2c1f6b1890ed255c8dd889ff29fcafc0");
 myHeaders.append("X-RapidAPI-Host", "book-finder1.p.rapidapi.com");
 
 var searchButton = document.getElementById("search-button")
