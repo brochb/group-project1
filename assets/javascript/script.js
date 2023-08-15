55c22466ca1ea224389492b693dafb9db711a85d
Richard-Au1 <136401843+Richard-Au1@users.noreply.github.com>
2023-08-15 14:18:05 -0600

    adjusted the clear history section to only be run on modal.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -222,12 +222,13 @@ searchButton.addEventListener('click', function () {
     localStorage.removeItem('Current Page');
 });
 
-// Add event listener for the "Clear Selection" button
-var clearSelectionButton = document.getElementById("clear-selection-button");
-clearSelectionButton.addEventListener("click", function () {
+// // Add event listener for the "Clear Selection" button
+// var clearSelectionButton = document.getElementById("clear-selection-button");
+// clearSelectionButton.addEventListener("click", function () {
 
-    localStorage.removeItem('selectedBooks');
-    // Refresh the page to display the recently cleared storage
-    window.location.reload();
-});
+    
+//     localStorage.removeItem('selectedBooks');
+//     // Refresh the page to display the recently cleared storage
+//     // window.location.reload();
+// });
 
